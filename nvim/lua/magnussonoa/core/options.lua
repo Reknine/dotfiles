2ab2e3d68e1ef0b3998c898e453260b3ba6a792e
Ola Magnusson <oulamag@gmail.com>
2024-09-22 13:46:57 +0200

    added updates to options.lua for nvim

--- nvim/lua/magnussonoa/core/options.lua
+++ nvim/lua/magnussonoa/core/options.lua
@@ -1,5 +1,7 @@
 local opt = vim.opt -- for conciseness
 
+opt.guicursor = ""
+
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
@@ -9,6 +11,7 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
@@ -18,7 +21,7 @@ opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+opt.cursorline = false -- highlight the current cursor line
 
 -- appearance
 
@@ -54,3 +57,14 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+opt.colorcolumn = "80"
+
